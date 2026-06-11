ff7149ed0a9aebdd42130f0012f7e1c8b353c910
Aaron L. Zeng <me@bcc32.com>
2026-06-10 23:08:11 -0400

    cherry-pick rework-configuration-layer-used-p
    Squashed commit of the following:
    
    commit 21c60c3f141a6623a4f2d52fb6cf5e048b08973e
    Author: Aaron L. Zeng <me@bcc32.com>
    Date:   Sat Mar 1 15:36:37 2025 -0500
    
        [spacemacs-org] Disable org-superstar when org-modern is enabled
    
        org-modern provides similar functionality for the leading stars in org
        headlines and it is recommended to disable org-superstar when
        org-modern is used.
    
    commit 5ec897a7e5002fea17748cfe16eadbe7cc516248
    Author: Aaron L. Zeng <me@bcc32.com>
    Date:   Sat Mar 22 01:30:49 2025 -0400
    
        WIP lots of code review
    
    commit 0cb468c9481990b9c88ede408bd8b0b9b91252a8
    Author: Aaron L. Zeng <me@bcc32.com>
    Date:   Tue Mar 18 00:32:52 2025 -0400
    
        [layouts] Remove workaround for configuration-layer/package-used-p
    
        Now that configuration-layer/package-used-p checks the package :toggle
        form, there is no need to also check the mode line theme name.
    
    commit 0ff7a6647f4d4f47fb453b0c104c216e0ece5b63
    Author: Aaron L. Zeng <me@bcc32.com>
    Date:   Tue Apr 2 23:04:27 2024 -0400
    
        configuration-layer/package-used-p: Check package :toggle non-nil
    
        This makes configuration-layer/package-used-p behave the same as
        cfgl-package-used-p for the same package, which should have been the
        case all along.
    
    commit 0ebfb5dcbf305b5255f30b18f2a9ccc19a5eadb9
    Author: Aaron L. Zeng <me@bcc32.com>
    Date:   Tue Apr 2 23:48:25 2024 -0400
    
        cfgl-package-reqs-satisfied-p: Check if required package is used
    
        A package's dependencies are only satisfied if the dependency is
        actually used (:toggle evaluates to non-nil).  If the dependency is
        :toggled off, then the dependent package will not be installed or
        used, so this function should reflect that.

--- core/core-configuration-layer.el
+++ core/core-configuration-layer.el
@@ -317,14 +317,14 @@ is ignored."
     (eval toggle)))
 
 (cl-defmethod cfgl-package-reqs-satisfied-p ((pkg cfgl-package) &optional inhibit-messages)
-  "Check if requirements of a package are all enabled.
+  "Check if requirements of a package are all used.
 If INHIBIT-MESSAGES is non nil then any message emitted by the toggle evaluation
 is ignored."
   (cl-every
    (lambda (dep-pkg)
      (let ((pkg-obj (configuration-layer/get-package dep-pkg)))
        (when pkg-obj
-         (cfgl-package-enabled-p pkg-obj inhibit-messages))))
+         (cfgl-package-used-p pkg-obj inhibit-messages))))
    (oref pkg requires)))
 
 (cl-defmethod cfgl-package-enabled-p ((pkg cfgl-package) &optional inhibit-messages)
@@ -404,6 +404,7 @@ file. It can be overridden by users inside `dotspacemacs/user-init'.")
 (defvar configuration-layer--lazy-mode-alist nil
   "Association list where the key is a mode and the value a regexp.")
 
+;; TODO: no code sets this variable, can we get rid of it?
 (defvar configuration-layer--inhibit-errors nil
   "If non-nil then error messages emitted by the layer system are ignored.")
 
@@ -425,6 +426,7 @@ installation of initialization.")
   "List of strings corresponding to category names. A category is a
 directory with a name starting with `+'.")
 
+;; FIXME: bad namespace
 (defvar update-packages-alist '()
   "List used to collect information about rollback packages in the
 cache folder.
@@ -502,6 +504,7 @@ Otherwise return the recipe unchanged.  PKG is of `cfgl-package' type."
         (eq (string-match-p "^[a-zA-Z]:" path) 0)
         (string-prefix-p "\." path))))
 
+;; FIXME: this seems dumb, we should just require ARCHIVES to include schemes.
 (defun configuration-layer//resolve-package-archives (archives)
   "Resolve HTTP handlers for each archive in ARCHIVES and return a list
 of all reachable ones.
@@ -1124,10 +1127,12 @@ USEDP non-nil means that PKG is a used layer."
     (when usedp
       (add-to-list 'configuration-layer--used-layers layer-name))))
 
+;; FIXME: unused
 (defun configuration-layer/remove-layers (layer-names)
   "Remove layers with LAYER-NAMES from used layers."
   (mapc 'configuration-layer/remove-layer layer-names))
 
+;; FIXME: only one use-case, maybe we can refactor it away
 (defun configuration-layer/remove-layer (layer-name)
   "Remove an used layer with name LAYER-NAME."
   (setq configuration-layer--used-layers
@@ -1488,6 +1493,9 @@ If `SKIP-LAYER-DEPS' is non nil then skip loading of layer dependenciesl"
             (configuration-layer//load-layer-files layer-name '("layers"))))
       (configuration-layer//warning "Unknown declared layer %s." layer-name))))
 
+;; FIXME: some layers use this and some layers use
+;; configuration-layer/declare-layers.  what's the difference, can we unify
+;; them?
 (defun configuration-layer/declare-layer-dependencies (layer-names)
   "Function to be used in `layers.el' files to declare dependencies."
   (dolist (x layer-names)
@@ -1618,12 +1626,8 @@ RNAME is the name symbol of another existing layer."
 
 (defun configuration-layer/package-used-p (name)
   "Return non-nil if NAME is the name of a used package."
-  (let ((obj (configuration-layer/get-package name)))
-    (and obj (cfgl-package-get-safe-owner obj)
-         (not (oref obj excluded))
-         (not (memq nil (mapcar
-                         'configuration-layer/package-used-p
-                         (oref obj requires)))))))
+  (when-let* ((obj (configuration-layer/get-package name)))
+    (cfgl-package-used-p obj t)))
 
 (defalias 'configuration-layer/package-usedp
   'configuration-layer/package-used-p)
@@ -1739,7 +1743,8 @@ RNAME is the name symbol of another existing layer."
                                    "layer %s, do you want to install it?")
                            mode layer-name))))
     (when (dotspacemacs/add-layer layer-name)
-      (let (spacemacs-sync-packages)
+      (let (spacemacs-sync-packages)    ;FIXME: unused lexical variable.  is
+                                        ;this working?
         (configuration-layer/load)))
     (let* ((layer (configuration-layer/get-layer layer-name))
            (inst-pkgs
@@ -1950,6 +1955,8 @@ RNAME is the name symbol of another existing layer."
                ((eq step 'pre) pre-packages)
                (t other-packages)))))
 
+    ;; FIXME: this order should probably not matter but some how it errors if I
+    ;; don't have these reverses.
     (setq bootstrap-packages (nreverse bootstrap-packages))
     (setq pre-packages (nreverse pre-packages))
     (setq other-packages (nreverse other-packages))
@@ -2406,6 +2413,7 @@ Rollback slots are stored in
 
 The keys are package names and the values are lists of package names that
 depends on it."
+  ;; TODO: test this refactor
   (let ((result (make-hash-table)))
     (dolist (pkg package-alist)
       (let* ((pkg-sym (car pkg))
@@ -2565,14 +2573,13 @@ Return nil if MODE does not appear in `auto-mode-alist'."
 
 (defun configuration-layer//insert-lazy-install-form (layer-name mode ext)
   "Insert a configuration form for lazy installation of MODE."
-  (let ((str (concat "(configuration-layer/lazy-install '"
-                     (symbol-name layer-name)
-                     " :extensions '("
-                     (let ((print-quoted t)) (prin1-to-string ext))
-                     " "
-                     (symbol-name mode)
-                     "))\n")))
-    (insert str)))
+  (insert "(configuration-layer/lazy-install '"
+          (symbol-name layer-name)
+          " :extensions '("
+          (let ((print-quoted t)) (prin1-to-string ext))
+          " "
+          (symbol-name mode)
+          "))\n"))
 
 (defun configuration-layer/insert-lazy-install-configuration ()
   "Prompt for a layer and insert the forms to configure lazy installation."
@@ -2629,6 +2636,7 @@ Return nil if MODE does not appear in `auto-mode-alist'."
                            (lambda (x) (assq x package-archive-contents))
                            deps)))
           (dolist (pkg (cons pkg-sym elpa-deps))
+            ;; FIXME: but pkg-sym should surely be unique?
             ;; avoid duplicates
             (cl-pushnew pkg result)))))
     result))
@@ -2866,6 +2874,7 @@ continue with the stable ELPA repository installation."
      (t t))))
 
 (defun configuration-layer//stable-elpa-untar-archive ()
+  ;; FIXME: docstring is wrong about return value of `call-process'
   "Untar the downloaded archive of stable ELPA, returns non-nil if succeeded."
   (require 'tar-mode)
   (let ((archive (configuration-layer//stable-elpa-tarball-local-file))
@@ -2941,6 +2950,7 @@ files."
 ;;  "spacelpa"
 ;;  spacemacs-cache-directory)
 
+;; FIXME: initialize to 0 and just use cl-incf
 (defun configuration-layer//increment-error-count ()
   "Increment the error counter."
   (if configuration-layer-error-count
@@ -2948,6 +2958,7 @@ files."
             (1+ configuration-layer-error-count))
     (setq configuration-layer-error-count 1)))
 
+;; FIXME: format-message
 (defun configuration-layer/message (msg &rest args)
   "Display MSG in *Messages* prepended with '(Spacemacs)'.
 ARGS: format string arguments."
